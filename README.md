5d51e91f312be581abfdd1de411fbc57760294f9
swlincode <pgcode84@gmail.com>
2017-01-23 10:46:22 +0800

    markdown

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # standard-deviation
-py_midterm_02
+
+**Standard Deviation Formula:**
+Reference:[mathsisfun.com](https://www.mathsisfun.com/data/standard-deviation-formulas.html) 
+- Step 1: Mean of all values (mu).
+- Step 2: For each number, subtract the mean and square the result.
+- Step 3: Add up all the values, then divide by how many.
+- Step 4: Square root.'''
--- my_sd_function.py
+++ my_sd_function.py
@@ -1,10 +1,3 @@
-'''Standard Deviation Formula:
-Reference: https://www.mathsisfun.com/data/standard-deviation-formulas.html
-Step 1: Mean of all values (mu).
-Step 2: For each number, subtract the mean and square the result.
-Step 3: Add up all the values, then divide by how many.
-Step 4: Square root.'''
-
 import math, random
 
 class MyMath:
@@ -12,19 +5,19 @@ class MyMath:
         self.values = values  
 
     def sd_func(self):
-        sum_values = 0  # Step 1
+        sum_values = 0
         for i in self.values:
             sum_values += i
         number_values = len(self.values)
         mean_values = sum_values / number_values
 
         sum_count_values = 0
-        for j in self.values:  # Step 2
+        for j in self.values:
             sum_count_values += (j - mean_values) ** 2
-        variance = sum_count_values / number_values  # Step 3
-        sd_result = math.sqrt(variance) # Step 4
+        variance = sum_count_values / number_values
+        sd_result = math.sqrt(variance)
         return sd_result
 
-random_range_one_five = random.sample(range(1, 5), 3)
+random_range_one_five = (random.sample(range(1, 5), 3))
 random_values = MyMath(random_range_one_five)
 print(random_values.values, "\n", random_values.sd_func())
